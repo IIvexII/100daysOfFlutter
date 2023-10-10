88505e8c7f1857fc0f116bf662eb7efa77704245
Zafeer Hafeez <zafeerhafeez1738@gmail.com>
2023-10-11 04:19:45 +0500

    refactor: Gradiant Colors are dynamic now

--- role_dice/lib/gradient_container.dart
+++ role_dice/lib/gradient_container.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:role_dice/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: <Color>[
-            Color.fromARGB(255, 18, 8, 111),
-            Color.fromARGB(255, 31, 27, 150),
-          ],
+          colors: colors,
           begin: Alignment.bottomRight,
           end: Alignment.topLeft,
         ),
--- role_dice/lib/main.dart
+++ role_dice/lib/main.dart
@@ -4,7 +4,12 @@ import 'package:role_dice/gradient_container.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: GradientContainer(),
+      body: GradientContainer(
+        colors: <Color>[
+          Color.fromARGB(255, 84, 6, 6),
+          Color.fromARGB(255, 144, 24, 40),
+        ],
+      ),
     ),
     // Remove the debug banner
     debugShowCheckedModeBanner: false,
