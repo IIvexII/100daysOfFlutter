d8b5a334653d2a099d947b4132295ed658230f85
Zafeer Hafeez <zafeerhafeez1738@gmail.com>
2023-10-11 03:34:00 +0500

    refactpr: styled text into its own widget

--- role_dice/lib/gradient_container.dart
+++ role_dice/lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:role_dice/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -17,13 +18,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text(
-          '#100DayChallenge',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
+        child: StyledText(data: "#100DaysOfFlutter"),
       ),
     );
   }
--- role_dice/lib/main.dart
+++ role_dice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:role_dice/gradient_container.dart';
 
 void main() {
   runApp(const MaterialApp(
