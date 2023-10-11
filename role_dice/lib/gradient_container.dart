49e2647418a5fb3eea53e4aff52f75a49e9cd498
Zafeer Hafeez <zafeerhafeez1738@gmail.com>
2023-10-12 01:36:24 +0500

    feat: roll dice on tap anywhare

--- role_dice/lib/gradient_container.dart
+++ role_dice/lib/gradient_container.dart
@@ -1,24 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class GradientContainer extends StatelessWidget {
+class GradientContainer extends StatefulWidget {
   const GradientContainer({super.key, required this.colors});
-
   final List<Color> colors;
 
   @override
+  State<GradientContainer> createState() {
+    return _GradientContainer();
+  }
+}
+
+class _GradientContainer extends State<GradientContainer> {
+  int currentDiceRoll = 3;
+
+  void diceRollAnimation() {
+    for (int i = 0; i < 10; i++) {
+      Future.delayed(
+        Duration(milliseconds: 100 * i),
+        () {
+          setState(() {
+            currentDiceRoll = Random().nextInt(6) + 1;
+          });
+        },
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: colors,
-          begin: Alignment.bottomRight,
-          end: Alignment.topLeft,
+    return InkWell(
+      onTap: () {
+        diceRollAnimation();
+        setState(() {
+          currentDiceRoll = Random().nextInt(6) + 1;
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: widget.colors,
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+          ),
         ),
-      ),
-      child: Center(
-        child: Image.asset(
-          'assets/images/dice-6.png',
-          width: 200,
+        child: Center(
+          child: Image.asset(
+            'assets/images/dice-$currentDiceRoll.png',
+            width: 200,
+          ),
         ),
       ),
     );
--- role_dice/lib/main.dart
+++ role_dice/lib/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:role_dice/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: GradientContainer(
-        colors: <Color>[
-          Color.fromARGB(255, 84, 6, 6),
-          Color.fromARGB(255, 144, 24, 40),
-        ],
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          colors: <Color>[
+            Color.fromARGB(255, 84, 6, 6),
+            Color.fromARGB(255, 144, 24, 40),
+          ],
+        ),
       ),
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
     ),
-    // Remove the debug banner
-    debugShowCheckedModeBanner: false,
-  ));
+  );
 }
