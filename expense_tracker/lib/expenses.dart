c8068f77715a9a5dcec4ba3eb81c22ab84ee96c5
Zafeer Hafeez <contact@zafeer.pk>
2023-11-10 09:13:22 +0500

    feat: add dummy data to work with

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expenses.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,27 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Pizza Party",
+      amount: 3200,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Petrol for Motorbike",
+      amount: 1000,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: "New Laptop: HP",
+      amount: 20000,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
